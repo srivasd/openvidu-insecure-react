d8dbd2cb2d99ba43c719970dab99d686fda44b37
srivasd <srivasdelgado@gmail.com>
2018-01-13 14:26:49 +0100

    OpenVidu browser 1.4.0 version updated

--- src/StreamComponent.js
+++ src/StreamComponent.js
@@ -14,20 +14,22 @@ export default class StreamComponent extends Component {
     }
 
     this.handleVideoClicked = this.handleVideoClicked.bind(this);
-    this.myVideoSrc = props.stream.videoSrcObject;
 
     var that = this;
 
     var intervalSrc = setInterval(function(){
       if(that.state!==undefined){
-        if(props.stream.videoSrcObject!==undefined){
-          var src = URL.createObjectURL(props.stream.videoSrcObject);
-          if (!(that.state.videoSrcUnsafe === src)) {
-            that.setState({
-              videoSrc: src,
-              videoSrcUnsafe: src
-            });
-            clearInterval(intervalSrc);
+        if(props.stream.video.srcObject!==undefined){
+          if(props.stream.video.srcObject!==null){
+            console.log(props.stream.video.srcObject);
+            var src = URL.createObjectURL(props.stream.video.srcObject);
+            if (!(that.state.videoSrcUnsafe === src)) {
+              that.setState({
+                videoSrc: src,
+                videoSrcUnsafe: src
+              });
+              clearInterval(intervalSrc);
+            }
           }
         }
       }
@@ -37,13 +39,15 @@ export default class StreamComponent extends Component {
   componentWillReceiveProps(nextProps){
     var that = this;
     if(that.state!==undefined){
-      if(nextProps.stream.videoSrcObject!==undefined){
-        var src = URL.createObjectURL(nextProps.stream.videoSrcObject);
-        if (!(that.state.videoSrcUnsafe === src)) {
-          that.setState({
-            videoSrc: src,
-            videoSrcUnsafe: src
-          });
+      if(nextProps.stream.video.srcObject!==undefined){
+        if(nextProps.stream.video.srcObject!==null){
+          var src = URL.createObjectURL(nextProps.stream.video.srcObject);
+          if (!(that.state.videoSrcUnsafe === src)) {
+            that.setState({
+              videoSrc: src,
+              videoSrcUnsafe: src
+            });
+          }
         }
       }
     }
